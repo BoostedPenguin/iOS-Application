8174f3f81af31a6d170a660eca04a3a123e48728
Aleksandar Todorov <alex20009@abv.bg>
2021-05-19 17:16:35 +0200

    Initial Commit

---
+++ smth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  smth
+//
+//  Created by User on 5/19/21.
+//  Copyright © 2021 User. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
